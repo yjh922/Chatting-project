c12c6ba9afcb314e7400ae5b02a64c726836bee4
yjh922 <jihyun648@naver.com>
2023-08-03 11:36:13 +0900

    1

--- src/org/sp/chat/client/view/Page.java
+++ src/org/sp/chat/client/view/Page.java
@@ -3,20 +3,41 @@ package org.sp.chat.client.view;
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.Font;
 
+import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
 import javax.swing.border.LineBorder;
 
 public class Page extends JPanel{
 	JPanel p_north;
 	JPanel p_center;
+	JLabel la_title;
+	JLabel la_plus;
 
 	public Page() {
 		p_north = new JPanel();
 		p_center=new JPanel();
+		la_title = new JLabel("");
+		la_plus = new JLabel("");
 		
+		//스타일
 		p_north.setBorder(new LineBorder(Color.LIGHT_GRAY, 2));
 		p_north.setPreferredSize(new Dimension(330,50));
+		Dimension d = new Dimension(140,40);
+		la_title.setPreferredSize(d);
+		la_plus.setPreferredSize(d);
+		Font f =new Font("돋움", Font.BOLD, 20);
+		la_title.setFont(f);
+		la_plus.setFont(f);
+		
+		la_title.setHorizontalAlignment(JLabel.LEFT);
+		la_plus.setHorizontalAlignment(JLabel.RIGHT);
+		
+		//조립
+		p_north.add(la_title);
+		p_north.add(la_plus);
 		
 		setLayout(new BorderLayout());
 		
